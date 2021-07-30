e6c5c71b8dca08600214ec8d6c764581527123a9
Vladimir Tikhomirov <vladimir.tikhomirov@op.fi>
2021-07-30 15:42:06 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+import {InvestmentAnalyzer} from "./investment-analyzer";
+
+function start() {
+    const investmentAnalyzer = new InvestmentAnalyzer();
+    investmentAnalyzer.run()
+}
+
+start();
